11f29444112d2e1a7f8e2214d4537c6db398bb6a
rohit <rohitkr23072001@gmail.com>
2025-01-05 16:39:59 +0530

    fix css

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,41 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import { Button } from './components/ui/button'
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
- 
-       
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
+      <div className="card ">
+      <Button    onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button> <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button> <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button> <Button onClick={() => setCount((count) => count + 1)}>
+        <Button
+          variant={"destructive"}
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </Button>
-       </div>
+        <Button
+          variant={"outline"}
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </Button>
+        <Button   variant={"secondary"} onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+        <Button   variant={"ghost"} onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+        <Button   variant={"link"} onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
